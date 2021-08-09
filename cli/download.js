dc8159cd6999c9ce0b2775da7a3cc4ce16ec5a27
pixelsquare <anthony.ganzon@ymail.com>
2021-08-09 10:54:17 +0800

    Code implementation file

--- cli/download.js
+++ cli/download.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const path = require('path');
 const YTDownloader = require('./index.js');
 const downloader = new YTDownloader();
 
-downloader.init('-url https://www.youtube.com/watch?v=_7unv8Z0oLw -o ' + path.join(__dirname, '/output'));
-downloader.download('https://www.youtube.com/watch?v=_7unv8Z0oLw', function(result) {
-    console.log(result);
+downloader.init('-url https://www.youtube.com/watch?v=_7unv8Z0oLw').then(instance => {
+    downloader.download().then(result => {
+        console.log(result);
+    });
 });
-// downloader.chooseFormat('https://www.youtube.com/watch?v=_7unv8Z0oLw');
