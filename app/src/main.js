c55ba87d4fde0a8420b7905d1f8320592f1e2ca1
pixelsquare <anthony.ganzon@ymail.com>
2021-08-23 12:00:23 +0800

    Removed menu from electron window

--- app/src/main.js
+++ app/src/main.js
@@ -28,9 +28,7 @@ const createWindow = () => {
   // Open the DevTools.
   mainWindow.webContents.openDevTools({ mode: 'detach' });
 
-  if(isDev) {
-    Menu.setApplicationMenu(null);
-  }
+  Menu.setApplicationMenu(null);
 };
 
 // This method will be called when Electron has finished
